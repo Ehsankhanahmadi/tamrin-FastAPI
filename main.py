81cc05fb4f88bf16c1adffc4333437ae31517e8d
ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2026-06-18 15:27:02 +0330

    tamrin path function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Path
 from fastapi.responses import JSONResponse
 # import uvicorn
+from typing import Annotated
 
 app = FastAPI()
 
@@ -9,7 +10,7 @@ async def startup_event():
     return {"Message:": "Hello World"}
 
 @app.get("/course/desc/seo")
-async def testpathfunc():
+async def testparamsfunc():
     return JSONResponse(content={"message":"description test for seo"})
 
 @app.get("/course/{id}/{name}")
@@ -17,6 +18,9 @@ async def pathfunction(id:int,name:str):
 # async def pathfunction(id:str,name:str):
     return JSONResponse(status_code=200,content={"message":f"{id} and {name}"})
 
+@app.get('/post/{id}')
+async def testpathfunc(id:Annotated[int,Path(lt=5,description="should enter letter than 5 number")]):
+    return JSONResponse(status_code=200 , content={"post":f"{id}"})
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app",host="127.0.0.1",reload=True)
