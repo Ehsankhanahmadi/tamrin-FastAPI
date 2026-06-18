0d425ed6bd9b7a8a93fb3a30bd714caf6ab35998
ehsankhanahmadi <ehsankhanahmadi.web@gmail.com>
2026-06-18 16:57:52 +0330

    tamrin path parameter and function parameter and query parameter

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI , Path
+from fastapi import FastAPI , Path, Query
 from fastapi.responses import JSONResponse
 # import uvicorn
 from typing import Annotated
 
 app = FastAPI()
+
+data = [
+    {
+        "title":"python for beginer",
+        "decs":"test for one data"
+    },
+    {
+        "title":"js for beginer",
+        "decs":"test for two data"
+    },
+    {
+        "title":"java for beginer",
+        "decs":"test for three data"
+    },
+]
 
 @app.get("/")
 async def startup_event():
@@ -21,6 +36,12 @@ async def pathfunction(id:int,name:str):
 @app.get('/post/{id}')
 async def testpathfunc(id:Annotated[int,Path(lt=5,description="should enter letter than 5 number")]):
     return JSONResponse(status_code=200 , content={"post":f"{id}"})
+
+@app.get('/data')
+async def queryfunc (find:Annotated[list[str],Query(description="find data with filter find")] = []):
+    if find:
+        result = [item for item in data if any(i for i in find if i.lower() in item["title"].lower())]
+        return JSONResponse(content=result,status_code=200)
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app",host="127.0.0.1",reload=True)
